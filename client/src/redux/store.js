e34cf50c606364b5e81f923f76ee938688d65fd4
Facundo Frias <97521604+FacundoEFrias@users.noreply.github.com>
2023-09-14 12:18:29 +0200

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,11 +3,11 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducer.js';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   
 );
 
